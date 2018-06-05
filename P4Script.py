77fed99aaf0720f9c72c7d2507bd366133b2f5b6
Wenqi <guowenqi1983@hotmail.com>
2018-06-05 23:17:45 +0800

    delete the original p4 script

--- P4Script.py
+++ P4Script.py
@@ -1,8 +1,27 @@
-from P4 import P4
 
+from P4 import P4, P4Exception
+# set up instance
 p4 = P4()
-p4.user = "alvin.guo"
-p4.password = "Enternal#831218"
-p4.connect()
-p4.run_login()
-opened = p4.run_opened()
+
+# Try to get the client info, like client root attribute
+# p4.client = 'alvin.guo.laptop'
+# p4.connect()
+# client = p4.fetch_client()
+# print(client)
+# p4.disconnect()
+
+# Start to sync the special path from branch
+try:
+    p4.client = 'alvin.guo_xxx-IDL276_6020'
+    p4.user = 'alvin.guo'
+    p4.port = 'perforce:1666'
+    # p4.password = ''
+    p4.exception_level = 1
+    p4.connect()
+    for syncFileInfo in p4.run_sync("//depot/BuildSystem/Bin/Deployed/BuildSystemClient/...#head"):
+        print(syncFileInfo['clientFile'])
+except P4Exception:
+    for e in p4.errors:
+        print(e)
+finally:
+  p4.disconnect()
