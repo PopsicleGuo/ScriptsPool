5527b046af91e038557df47968ebdf42b1e09f5f
wenqi <guowenqi1983@hotmail.com>
2018-08-03 18:37:33 +0800

    Add new script for P4 API using

---
+++ Get_filePatch_from_depot.py
@@ -0,0 +1,53 @@
+
+from P4 import P4, P4Exception
+# set up instance
+p4 = P4()
+
+'''
+Try to get folder structure from //depot folder
+'''
+def getFolder():
+    try:
+        p4.client = 'EASH-wenqiguo-picinc-dev'
+        p4.user = 'EASAP\wenqiguo'
+        p4.port = 'vgsproxy04:1666'
+        # p4.password = ''
+        p4.exception_level = 1
+        p4.connect()
+        info = p4.run("dirs", "//pvz/*/*/*/*/*")
+        for i in info:
+            print(i)
+    except P4Exception:
+        for e in p4.errors:
+            print(e)
+    finally:
+        p4.disconnect()
+
+
+
+'''
+Try to get files from depot folder and output the result to another file
+'''
+def getFiles():
+    try:
+        p4.client = 'EASH-wenqiguo-picinc-dev'
+        p4.user = 'EASAP\wenqiguo'
+        p4.port = 'vgsproxy04:1666'
+        # p4.password = ''
+        p4.exception_level = 1
+        p4.connect()
+        info = p4.run("files", "//pvz/gw3/...#head")
+        f = open('p:\originallist.txt', 'w')
+        for i in info:
+            f.write(str(i['depotFile'] + "\n"))
+        f.close()
+    except P4Exception:
+        for e in p4.errors:
+            print(e)
+    finally:
+        p4.disconnect()
+
+
+# getFolder()
+
+getFiles()
---
+++ Get_filePatch_from_local.py
@@ -0,0 +1,4 @@
+import os
+
+def getFileloc():
+    root = "p:\p4\"
