69f678791fef4b4bb3bc8cf6a88191ad53b58020
wenqi <guowenqi1983@hotmail.com>
2018-08-24 12:16:31 +0800

    Added a script about how do we get the week number of the year

--- get_datetime.py
+++ get_datetime.py
@@ -2,16 +2,14 @@ from datetime import date
 
 
 def get_weeknumber():
-    today = date.today()
-    weeknumber = today.isocalendar()[1]
+    weeknumber = date.today().isocalendar()[1]
     print("This week is: ", weeknumber)
 
 # get_weeknumber()
 
 
 def get_specificnumber(year, months, day):
-    get_format = date(int(year), int(months), int(day))
-    get_wknumber = get_format.isocalendar()[1]
+    get_wknumber = date(int(year), int(months), int(day)).isocalendar()[1]
     print("This week number is: ", get_wknumber)
 
 get_specificnumber(2018, 10, 1)
